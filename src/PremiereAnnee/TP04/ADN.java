70736a82bd82ee5c98f5ff5430020e0c5a0e3c0a
LimeiloN <memorial.limelion@gmail.com>
2018-12-06 10:55:26 +0100

    Ajouté TP04 Bonus

---
+++ src/PremiereAnnee/TP04/ADN.java
@@ -0,0 +1,58 @@
+package PremiereAnnee.TP04;
+
+import java.util.Random;
+
+public class ADN {
+
+    // adénine (A), cytosine (C), guanine (G), thymine (T)
+    static char[] chars = "ACGT".toCharArray();
+
+    public static void main(String[] args) {
+        char[] brin1 = genBrin(24);
+        char[] brin2 = genBrin(24);
+
+        System.out.println("Brin1          : " + new String(brin1));
+        System.out.println("Brin2          : " + new String(brin2));
+
+        mutate(brin1);
+
+        System.out.println("Brin1 (mutated): " + new String(brin1));
+
+        char[] brin3 = combinate(brin1, brin2);
+
+        System.out.println("Brin3          : " + new String(brin3));
+    }
+
+    public static char[] genBrin(int size) {
+
+        char[] brin = new char[size];
+        Random rng = new Random();
+
+        for (int i = 0; i < size; i++) {
+            brin[i] = chars[rng.nextInt(4)];
+        }
+
+        return brin;
+    }
+
+    public static void mutate(char[] brin) {
+        Random rng = new Random();
+        brin[rng.nextInt(brin.length)] = chars[rng.nextInt(4)];
+    }
+
+    public static char[] combinate(char[] brin1, char[] brin2) {
+
+        int pos = new Random().nextInt(brin1.length - 1);
+        char[] newBrin = new char[brin1.length];
+
+        for (int i = 0; i < brin1.length; i++) {
+            if (i <= pos)
+                newBrin[i] = brin1[i];
+            if (i > pos)
+                newBrin[i] = brin2[i];
+        }
+
+        return newBrin;
+    }
+
+}
