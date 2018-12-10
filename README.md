0729e13202c1263c6e24687f9a56baacec4ea3a7
LimeiloN <memorial.limelion@gmail.com>
2018-12-10 13:51:28 +0100

    Mis à jour

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Ce repo contient tous les travaux dirigés fait en cours d'algorithmique à ce jour. Il sera mis à jour au fur et à mesure.
 
+Cette branche concerne la version 'Simple' des TD, càd en ne suivant que les instructions du cours. La branche 'Avancé' contient les corrections avec des fonctions plus poussées qui ne seront peut-être pas abordées en cours.
+
 Il contient pour l'instant :
  - 1A ("src/PremiereAnnee/)
    - TP01 : "src/PremiereAnnee/TP01"
--- src/PremiereAnnee/TP04/ADN.java
+++ src/PremiereAnnee/TP04/ADN.java
@@ -1,59 +1,117 @@
-package PremiereAnnee.TP04;
+import java.util.Scanner; // Pour les input
 
-import java.util.Random;
-
-public class ADN {
-
-    // adénine (A), cytosine (C), guanine (G), thymine (T)
-    static char[] chars = "ACGT".toCharArray();
-    static int brinSize = 20;
-
-    public static void main(String[] args) {
-        char[] brin1 = genBrin(brinSize);
-        char[] brin2 = genBrin(brinSize);
-
-        System.out.println("Brin1          : " + new String(brin1));
-        System.out.println("Brin2          : " + new String(brin2));
-
-        mutate(brin1);
-
-        System.out.println("Brin1 (mutated): " + new String(brin1));
-
-        char[] brin3 = combinate(brin1, brin2);
-
-        System.out.println("Brin3          : " + new String(brin3));
-    }
-
-    public static char[] genBrin(int size) {
-
-        char[] brin = new char[size];
-        Random rng = new Random();
-
-        for (int i = 0; i < size; i++) {
-            brin[i] = chars[rng.nextInt(4)];
+public class Adn {
+	
+	public static void main (String[] args) {
+		
+        final String delim = " "; // Délimitation lors de l'affichage des tableaux, initialiser à "\n" pour un retour à la ligne
+        
+        Scanner sc = new Scanner (System.in); // Iniitalisation du Scanner
+        final int nbVal = sc.nextInt(); // La taille du brin correspond à une entrée de l'utlisateur
+		
+		char[] adn = new char[nbVal]; // Initialisation du talbeau référençant les nucléotides du brin
+        char[] ref = {'A','C','T','G'}; // Tableau de référence utilisé pour convertir un nombre entre 1 et 3 en char correspondant à un nucléotide
+        int rand;
+        
+        
+        /** Init premier brin **/
+        
+        // Initialisation du tableau avec des nucléotides aléatoires
+        
+        for (int i=0;i<nbVal;i++) {
+            rand = (int)(Math.random()*4); // Nombre entier aléatoire entre 0 et 3 compris
+            adn[i] = ref[rand]; // On fait correspondre l'entier à un nucléotide
         }
+        
+        System.out.println("Premier brin");
+        affichageAdn(adn, delim); // Affichage du tableau grâce à la fonction affichageAdn définie plus bas
+        
+        
+        /** Création deuxième brin **/
+        
+        char[] adn2 = new char[nbVal]; // Nouveau tableau qui va contenir la séquence miroir
+        char copy;
+        
+        
+        // On parcours le tableau en remplaçant chaque nucléotide par son nucléotide miroir
+        
+        for (int i=0;i<nbVal;i++) {
+            copy = adn[i];
 
-        return brin;
-    }
-
-    public static void mutate(char[] brin) {
-        Random rng = new Random();
-        brin[rng.nextInt(brin.length)] = chars[rng.nextInt(4)];
-    }
-
-    public static char[] combinate(char[] brin1, char[] brin2) {
-
-        int pos = new Random().nextInt(brin1.length - 1);
-        char[] newBrin = new char[brin1.length];
-
-        for (int i = 0; i < brin1.length; i++) {
-            if (i <= pos)
-                newBrin[i] = brin1[i];
-            if (i > pos)
-                newBrin[i] = brin2[i];
+            if (copy == 'A')
+                adn2[i] = 'T';
+            if (copy == 'T')
+                adn2[i] = 'A';
+            if (copy == 'C')
+                adn2[i] = 'G';
+            if (copy == 'G')
+                adn2[i] = 'C';
         }
-
-        return newBrin;
+        
+        System.out.println("Brin miroir");
+        affichageAdn(adn2, delim); // Affichage du brin miroir
+        
+        
+        /** Mutation du brin **/
+        
+        char[] adnMute = new char[nbVal]; // Nouveau tableau pour stocker la séquence mutée
+        System.arraycopy(adn,0,adnMute,0, nbVal); // Copie du tableau adn dans adnMute, sans copier la référence
+        
+        int randPos = (int)(Math.random()*nbVal); // Potision de la mutation aléatoire, entre 0 et le dernier élément
+        int randNucl;
+        
+        // On s'assure que le nucléotide modifié n'est pas celui initial
+        
+        do {
+            randNucl = (int)(Math.random()*4); // Nucléotide aléatoire
+        }
+        while (adn[randPos] == ref[randNucl]); // On recommence si le nucléotide remplacé est celui initial
+        
+        adnMute[randPos] = ref[randNucl]; // On fait muter le nucléotide
+        
+        System.out.println("Brin modifié");
+        affichageAdn(adnMute,delim);
+        
+        
+        /** On crée un deuxième brin aléatoire de même taille **/
+        
+        // Même méthode que pour le brin initial
+        char[] adnB = new char[nbVal];
+        
+        for (int i=0;i<nbVal;i++) {
+            rand = (int)(Math.random()*4);
+            adnB[i] = ref[rand];
+        }
+        
+        System.out.println("Deuxième brin");
+        affichageAdn(adnB,delim);
+        
+        
+        /** Recombinaison de brin **/
+        
+        int randCut = (int)(Math.random()*(nbVal-2)); // Position de coupe aléatoire entre 0 et l'avant dernier élément; on coupe APRES cet élément
+        char[] adnRecomb = new char[nbVal];
+        
+        // Si i est plus grand que la position de coupe, le nucléotide du brin recombiné provient du second brin, et sinon, du premier brin
+        for (int i=0;i<nbVal;i++) {
+            if (i<=randCut) {
+                adnRecomb[i] = adn[i];
+            } else {
+                adnRecomb[i]= adnB[i];
+            }
+        }
+        
+        System.out.println("Brin recombiné, coupé en position " + randCut);
+        affichageAdn(adnRecomb,delim);
+	}
+    
+    
+    /** Méthode définie pour afficher un tableau donné en paramètre **/
+    public static void affichageAdn(char[] adn, String delim) {
+        for (int i=0;i<adn.length;i++) {
+            System.out.print(adn[i] + delim);
+        }
+        System.out.println("\n");
     }
-
 }
+
